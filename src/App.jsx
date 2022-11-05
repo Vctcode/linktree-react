36519e61e0926fd680258da448e13a7b3be6ff84
Vctcode <victorode01@gmail.com>
2022-11-05 05:27:22 +0100

    fix: 404 error from live server

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import { Link } from "react-router-dom"
-// Components
-import Home from "./components/Home"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+// // Components
+// import Home from "./components/Home"
+// import Contact from "./components/Contact"
+// import Footer from "./components/Footer"
 
 export default function App() {
   return (
     <div>
-      <Link to="/home">Home</Link>
-      <Link to="/contact">Contact</Link>
+      {/* <Link to="/Home">Home</Link>
+      <Link to="/contact">Contact</Link> */}
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route, } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import Home from './routes/Home'
-import Contact from './routes/Contact'
+import Contact from './routes/contact'
+import Footer from './components/Footer'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/contact'element={<Contact />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    <Footer />
+  </React.StrictMode>
 )
